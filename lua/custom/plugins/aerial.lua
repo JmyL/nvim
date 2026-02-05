13531aaabd54f1ed3799e998872d7cd4f7511527
Sungsik Nam <jmyl@me.com>
2026-02-05 11:01:38 +0100

    Add code outline plugin "aerial"

---
+++ lua/custom/plugins/aerial.lua
@@ -0,0 +1,14 @@
+return {
+  'stevearc/aerial.nvim',
+  opts = {},
+  -- Optional dependencies
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
+  },
+  init = function()
+    vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+    vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
+    vim.keymap.set('n', '<leader>v', '<cmd>AerialToggle left<CR>')
+  end,
+}
