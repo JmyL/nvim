d062f742ed35d28dbfd2d114fbc37266c7359103
Sungsik Nam <jmyl@me.com>
2026-03-06 11:27:49 +0100

    Update aerial shortcuts

--- lua/custom/plugins/aerial.lua
+++ lua/custom/plugins/aerial.lua
@@ -7,8 +7,8 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   init = function()
-    vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
-    vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
+    vim.keymap.set('n', '<C-{>', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+    vim.keymap.set('n', '<C-}>', '<cmd>AerialNext<CR>', { buffer = bufnr })
     vim.keymap.set('n', '<leader>v', '<cmd>AerialToggle left<CR>')
   end,
 }
