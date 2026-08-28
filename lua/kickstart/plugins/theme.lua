5961bff50807e571d0e2b66ae5eb2f79bb9b14bb
Sungsik Nam <jmyl@me.com>
2026-08-28 16:44:13 +0200

    Follow darkman with Catppuccin Latte and Macchiato.

--- lua/kickstart/plugins/theme.lua
+++ lua/kickstart/plugins/theme.lua
@@ -4,8 +4,57 @@ return {
     name = 'catppuccin',
     priority = 1000,
     config = function()
-      -- Load the colorscheme here.
-      vim.cmd.colorscheme 'catppuccin-macchiato'
+      local mode_file = vim.fn.expand '~/.cache/darkman/mode.txt'
+      local cache_dir = vim.fn.fnamemodify(mode_file, ':h')
+
+      local function background_from_darkman()
+        local file = io.open(mode_file, 'r')
+        if not file then
+          return 'dark'
+        end
+        local mode = (file:read '*l' or ''):gsub('%s+$', '')
+        file:close()
+        if mode == 'light' then
+          return 'light'
+        end
+        return 'dark'
+      end
+
+      local function apply_background()
+        local background = background_from_darkman()
+        if vim.o.background ~= background then
+          vim.o.background = background
+        end
+        vim.cmd.colorscheme 'catppuccin'
+      end
+
+      require('catppuccin').setup {
+        flavour = 'auto',
+        background = {
+          light = 'latte',
+          dark = 'macchiato',
+        },
+      }
+
+      vim.o.background = background_from_darkman()
+      vim.cmd.colorscheme 'catppuccin'
+
+      vim.uv.fs_mkdir(cache_dir, 448)
+      local watcher = vim.uv.new_fs_event()
+      if watcher then
+        watcher:start(
+          cache_dir,
+          {},
+          vim.schedule_wrap(function(err, filename)
+            if err then
+              return
+            end
+            if filename == nil or filename == 'mode.txt' then
+              apply_background()
+            end
+          end)
+        )
+      end
     end,
   },
 }
